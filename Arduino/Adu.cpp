3d458a7b2dca3d293804b3884750206a1b399071
alex@kernul.com <capnkernul@Alexander-Kerns-Mac.local>
2010-02-15 13:00:49 -0800

    Moved around some Adu functions

--- Arduino/Adu.cpp
+++ Arduino/Adu.cpp
@@ -1,5 +1,9 @@
 #include "Adu.h"
 
+//****************************************************************************
+// Getters
+//****************************************************************************
+
 uint8_t Adu::getSlaveId() { return slaveId; }
 uint8_t Adu::getFunction() { return function; }
 uint8_t Adu::getData() { return data; }
@@ -7,6 +11,43 @@ uint16_t Adu::getCrc() { return crc; }
 bool Adu::getRequest() { return request; }
 bool Adu::getResponse() { return !request; }
 bool Adu::getError() { return error; }
+
+//****************************************************************************
+// Setters
+//****************************************************************************
+
+void Adu::setSlaveId(uint8_t _slaveId) { slaveId = _slaveId; }
+void Adu::setData(uint8_t _data) { data = _data; }
+void Adu::setCrc(uint16_t _crc) { crc = _crc; }
+void Adu::setRequest(bool _request) { request = _request; }
+void Adu::setResponse(bool _response) { request = !_response; }
+
+//****************************************************************************
+// Computed Getters
+//****************************************************************************
+
+// Add 4 to the data bits needed to accomadate for the ADU's slave ID,
+// function or error code, and CRC.
+int Adu::getAduSize() {
+  const int bufferBytes = 4;
+  
+  int dataBits = dataBitsNeeded();
+  if(dataBits) { return bufferBytes + dataBits; }
+  return NULL;
+}
+
+// Retrieves the size of the data part of the ADU based on the function.
+int Adu::getDataSize() {
+  
+  // All errors only have the exception code byte as data.
+  if(error) { return 1; }
+  
+  // Implement the data bits needed for each function here.
+  if(WRITE_SINGLE_COIL) { return 4; }
+  
+  // Unknown function means we don't know how many bytes the data must be.
+  return NULL;
+}
 
 // Assembles all the various attributes into the final ADU
 uint8_t Adu::getAdu() {
@@ -17,12 +58,9 @@ uint8_t Adu::getAdu() {
     
     adu[0] = slaveId;
     
-    // Readds the error value if it's an error.
-    if(error) {
-      adu[1] = function + 0x80;
-    }else{
-      adu[1] = function;
-    }
+    // Re-adds the error value if it's an error.
+    adu[1] = function;
+    if(error) { adu[1] += 0x80; }
     
     // Places the data into the ADU with an offset of 2
     int dataSize = dataSize();
@@ -37,20 +75,13 @@ uint8_t Adu::getAdu() {
     return adu;
   }
   
+  // The ADU size wasn't able to be computed for some reason.
   return NULL;
 }
 
-uint8_t Adu::computeCrc() {
-  uint8_t adu = getAdu();
-  
-  for(i = 0; i < (sizeof(adu) - 2); i++) {
-    crc = _crc16_update(crc, adu[i]);
-  }
-  
-  return crc;
-}
-
-void Adu::setSlaveId(uint8_t _slaveId) { slaveId = _slaveId; }
+//****************************************************************************
+// Computed Setters
+//****************************************************************************
 
 // Checks if bit 7 (from the LSB) is a 1. This means that the function code
 // is an error code because it is over the value of 127.
@@ -66,37 +97,10 @@ void Adu::setFunction(uint8_t _function) {
   }
 }
 
-void Adu::setData(uint8_t _data) { data = _data; }
-void Adu::setCrc(uint16_t _crc) { crc = _crc; }
-void Adu::setRequest(bool _request) { request = _request; }
-void Adu::setResponse(bool _response) { request = !_response; }
-
 // Errors can only responses
 void Adu::setError(bool _error) {
   if(error == true) { request = false; }
   error = _error;
-}
-
-// Add 4 to the data bits needed to accomadate for the ADU's slave ID, function
-// or error code, and CRC.
-int Adu::aduSize() {
-  const int bufferBytes = 4;
-  
-  int dataBits = dataBitsNeeded();
-  if(dataBits) { return bufferBytes + dataBits; }
-  return NULL;
-}
-
-int Adu::dataSize() {
-  
-  // All errors only have the exception code byte as data.
-  if(error) { return 1; }
-  
-  // Implement the data bits needed for each function here.
-  if(WRITE_SINGLE_COIL) { return 4; }
-  
-  // Unknown function means we don't know how many bytes the data must be.
-  return NULL;
 }
 
 // Anything with an exception code must be a response.
