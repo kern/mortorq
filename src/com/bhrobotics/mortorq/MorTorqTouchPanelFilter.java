7f898b63e7a65fd5761b3c832a7ead05aa3354a9
Alexander Kern <alex@kernul.com>
2011-02-16 12:52:11 -0800

    Refactor the drive train code.

--- src/com/bhrobotics/mortorq/MorTorqTouchPanelFilter.java
+++ src/com/bhrobotics/mortorq/MorTorqTouchPanelFilter.java
@@ -49,9 +49,30 @@ public class MorTorqTouchPanelFilter extends TouchPanelFilter {
     }
     
     private class DriveTrainScreen extends TouchPanelScreen {
+        private static final int LEFT_FRONT_STOP = 4;
+        private static final int LEFT_FRONT_BACKWARD = 5;
+        private static final int LEFT_FRONT_SPEED = 12;
+        
+        private static final int RIGHT_FRONT_STOP = 6;
+        private static final int RIGHT_FRONT_BACKWARD = 7;
+        private static final int RIGHT_FRONT_SPEED = 13;
+        
+        private static final int LEFT_BACK_STOP = 8;
+        private static final int LEFT_BACK_BACKWARD = 9;
+        private static final int LEFT_BACK_SPEED = 14;
+        
+        private static final int RIGHT_BACK_STOP = 10;
+        private static final int RIGHT_BACK_BACKWARD = 11;
+        private static final int RIGHT_BACK_SPEED = 15;
+        
         public void bound() {
             System.out.println("[mortouch] Drive train screen bound.");
-            trigger("stopMotors");
+            
+            updateLeftFront();
+            updateRightFront();
+            updateLeftBack();
+            updateRightBack();
+            
             // trigger("mastStop");
             // trigger("compressorStop");
         }
@@ -60,112 +81,57 @@ public class MorTorqTouchPanelFilter extends TouchPanelFilter {
             String name = event.getName();
             
             if (name.equals("changeDigital4") || name.equals("changeDigital5") || name.equals("changeDigital12")) {
-                boolean digital4  = getDigital(4);
-                boolean digital5  = getDigital(5);
-                boolean digital12 = getDigital(12);
-                
-                double value;
-                
-                if (!digital4 && digital5) {
-                    value = 0.0;
-                } else if (digital4 && digital5) {
-                    value = -1.0;
-                } else {
-                    value = 1.0;
-                }
-                
-                if (digital12) {
-                    value /= 2.0;
-                }
-                
-                Hashtable data = new Hashtable();
-                data.put("value", new Double(value));
-                
-                trigger("changeMotorLeftFront", data);
+                updateLeftFront();
             }
             
             if (name.equals("changeDigital6") || name.equals("changeDigital7") || name.equals("changeDigital13")) {
-                boolean digital6  = getDigital(6);
-                boolean digital7  = getDigital(7);
-                boolean digital13 = getDigital(13);
-                
-                double value;
-                
-                if (!digital6 && digital7) {
-                    value = 0.0;
-                } else if (digital6 && digital7) {
-                    value = -1.0;
-                } else {
-                    value = 1.0;
-                }
-                
-                if (digital13) {
-                    value /= 2.0;
-                }
-                
-                Hashtable data = new Hashtable();
-                data.put("value", new Double(value));
-                
-                trigger("changeMotorRightFront", data);
+                updateRightFront();
             }
             
             if (name.equals("changeDigital8") || name.equals("changeDigital9") || name.equals("changeDigital14")) {
-                boolean digital8  = getDigital(8);
-                boolean digital9  = getDigital(9);
-                boolean digital14 = getDigital(14);
-                
-                double value;
-                
-                if (!digital8 && digital9) {
-                    value = 0.0;
-                } else if (digital8 && digital9) {
-                    value = -1.0;
-                } else {
-                    value = 1.0;
-                }
-                
-                if (digital14) {
-                    value /= 2.0;
-                }
-                
-                Hashtable data = new Hashtable();
-                data.put("value", new Double(value));
-                
-                trigger("changeMotorLeftBack", data);
+                updateLeftBack();
             }
             
             if (name.equals("changeDigital10") || name.equals("changeDigital11") || name.equals("changeDigital15")) {
-                boolean digital10 = getDigital(10);
-                boolean digital11 = getDigital(11);
-                boolean digital15 = getDigital(15);
-                
-                double value;
-                
-                if (!digital10 && digital11) {
-                    value = 0.0;
-                } else if (digital10 && digital11) {
-                    value = -1.0;
-                } else {
-                    value = 1.0;
-                }
-                
-                if (digital15) {
-                    value /= 2.0;
-                }
-                
-                Hashtable data = new Hashtable();
-                data.put("value", new Double(value));
-                
-                trigger("changeMotorRightBack", data);
+                updateRightBack();
             }
         }
-    }
-    
-    private boolean getDigital(int channel) {
-        try {
-            return ds.getDigital(channel);
-        } catch (DriverStationEnhancedIO.EnhancedIOException e) {
-            return false;
+        
+        private void updateLeftFront() {
+            updateMotor("LeftFront", LEFT_FRONT_STOP, LEFT_FRONT_BACKWARD, LEFT_FRONT_SPEED);
+        }
+        
+        private void updateRightFront() {
+            updateMotor("RightFront", RIGHT_FRONT_STOP, RIGHT_FRONT_BACKWARD, RIGHT_FRONT_SPEED);
+        }
+        
+        private void updateLeftBack() {
+            updateMotor("LeftBack", LEFT_BACK_STOP, LEFT_BACK_BACKWARD, LEFT_BACK_SPEED);
+        }
+        
+        private void updateRightBack() {
+            updateMotor("RightBack", RIGHT_BACK_STOP, RIGHT_BACK_BACKWARD, RIGHT_BACK_SPEED);
+        }
+        
+        private void updateMotor(String name, int stopChannel, int backwardChannel, int speedChannel) {
+            double value;
+            
+            if (getDigital(backwardChannel)) {
+                value = 1.0;
+            } else if (getDigital(stopChannel)) {
+                value = 0.0;
+            } else {
+                value = -1.0;
+            }
+            
+            if (!getDigital(speedChannel)) {
+                value /= 2.0;
+            }
+            
+            Hashtable data = new Hashtable();
+            data.put("value", new Double(value));
+            
+            trigger("changeMotor" + name, data);
         }
     }
     
@@ -192,6 +158,14 @@ public class MorTorqTouchPanelFilter extends TouchPanelFilter {
         
         public void handle(Event event) {
             
+        }
+    }
+    
+    private boolean getDigital(int channel) {
+        try {
+            return !ds.getDigital(channel);
+        } catch (DriverStationEnhancedIO.EnhancedIOException e) {
+            return false;
         }
     }
 }
